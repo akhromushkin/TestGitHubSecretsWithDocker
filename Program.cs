d22f639d711ba194b771f879061f432940d3de77
Alexander Khromushkin <alexander.khromushkin@devexpress.com>
2021-07-21 10:56:11 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using DevExpress.XtraRichEdit;
 
 namespace TestGitHubSecretsWithDocker {
     class Program {
         static void Main(string[] args) {
+            RichEditDocumentServer server = new RichEditDocumentServer();
             Console.WriteLine("Hello World!");
         }
     }
